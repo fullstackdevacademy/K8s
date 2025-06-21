95530e1e281a12c488e36851aaf7c4878c4ff916
daudulislamselise <daudul.islam@selise.ch>
2025-06-21 11:53:17 -0400

    K8s

---
+++ Enrollments/EnrollmentsApi/Context/EnrollmentsDbContext.cs
@@ -0,0 +1,12 @@
+﻿
+using EnrollmentsApi.Models;
+using Microsoft.EntityFrameworkCore;
+namespace EnrollmentsApi.Context
+{
+    public class EnrollmentsDbContext: DbContext
+    {
+        public EnrollmentsDbContext(DbContextOptions<EnrollmentsDbContext> options) : base(options) { }
+
+        public DbSet<UserAccount> UserAccounts { get; set; }
+    }
+}
